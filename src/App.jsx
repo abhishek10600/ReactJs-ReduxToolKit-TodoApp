28c4149fc06ca83210390f3fc91b70eaee98d7ad
abhishek10600 <abhi10sheksharma@gmail.com>
2023-09-29 21:20:26 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div>
+      <AddTodo />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
+
+const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>Delete</button>
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default Todos;
